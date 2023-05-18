7f08a5e89e93533bb9f10731fb164c1b5b401d2c
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2023-05-18 18:22:55 +0200

    Dodavanje starih skripti

---
+++ SQL-2021/Uvod-vjezbe/Vjezba-eclipse(ljeto)/Edunova01/src/Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Edunova");
+		System.out.println("Osijek");
+		System.out.println("Zagreb");
+		System.out.println("Practice");
+		System.out.println("Vjezbanje");
+	}
+
+}
